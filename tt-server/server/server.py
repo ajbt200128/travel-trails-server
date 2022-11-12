2143e6e74d255c59a7205cf79ed56552b28c469b
ajbt200128 <austin@cutedogs.org>
2022-11-12 15:57:07 -0500

    Upload image

--- tt-server/server/server.py
+++ tt-server/server/server.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify
+import uuid
+from pathlib import Path
+
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from server.database import db
 from server.models import *  # NOQA
@@ -9,6 +12,7 @@ DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+app.config["UPLOAD_FOLDER"] = "/data"
 
 db.init_app(app)
 
@@ -17,4 +21,23 @@ migrate = Migrate(app, db)
 
 @app.route("/hello")
 def hello():
-    return jsonify({"message": "Hello, World!"})
+    return jsonify({"message": "Hello, World!"}), 200
+
+
+@app.route("/upload_image/<location_id>", methods=["POST"])
+def upload_file(location_id):
+    uploaded_file = request.files["file"]
+    if uploaded_file:
+        file_uuid = str(uuid.uuid4())
+        path = Path(app.config["UPLOAD_FOLDER"]) / "images" / location_id
+        path.mkdir(parents=True, exist_ok=True)
+
+        uploaded_file.save(path / file_uuid)
+        return (
+            jsonify(
+                {"success": True, "message": f"saved {location_id} to {file_uuid}"}
+            ),
+            200,
+        )
+    else:
+        return jsonify({"success": False, "message": "no file"}), 400
