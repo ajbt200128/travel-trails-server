84aec8936cde5cbbc81c1107092e5a707e3c2b44
Jonathan Lee <jonathanlee@coltrane.cs.seas.gwu.edu>
2022-11-12 15:17:52 -0500

    added genmodel endpoint

--- tt-server/server/server.py
+++ tt-server/server/server.py
@@ -1,3 +1,4 @@
+import pycolmap
 from flask import Flask, jsonify
 from flask_migrate import Migrate
 from server.database import db
@@ -18,3 +19,23 @@ migrate = Migrate(app, db)
 @app.route("/hello")
 def hello():
     return jsonify({"message": "Hello, World!"})
+
+
+@app.route("/genmodel")
+def colmap():
+    image_dir = "/var/travel-trails-files/images/gerrard/"
+    output_path = "/var/travel-trails-files/models/gerrard"
+
+    output_path.mkdir()
+    mvs_path = output_path / "mvs"
+    database_path = output_path / "database.db"
+
+    pycolmap.extract_features(database_path, image_dir)
+    pycolmap.match_exhaustive(database_path)
+    maps = pycolmap.incremental_mapping(database_path, image_dir, output_path)
+    maps[0].write(output_path)
+    # dense reconstruction
+    pycolmap.undistort_images(mvs_path, output_path, image_dir)
+    pycolmap.patch_match_stereo(mvs_path)  # requires compilation with CUDA
+    pycolmap.stereo_fusion(mvs_path / "dense.ply", mvs_path)
+    return jsonify({"message": "Generated model successfully!"})
