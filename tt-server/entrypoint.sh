6bd83479e94296bb3b008493edd1c4f64103344c
ajbt200128 <austin@cutedogs.org>
2023-03-04 16:18:45 -0500

    Add fix again

--- tt-server/entrypoint.sh
+++ tt-server/entrypoint.sh
@@ -3,4 +3,4 @@
 export FLASK_APP=server.server:app
 while ! echo exit | nc localhost 5432; do sleep 10; done
 flask db upgrade
-gunicorn --worker-class gevent --workers=1 --max-requests 10000 --bind 0.0.0.0:8080 server.server:app
+gunicorn --worker-class gevent --workers=1 --max-requests 10000 -t 86400 --bind 0.0.0.0:8080 server.server:app
